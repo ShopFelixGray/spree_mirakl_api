5df7191282c6bba15c342e3f3ed63749732abedc
Mike Walters <mikewalters015@gmail.com>
2020-05-07 14:37:41 -0400

    Build get shipping orders

---
+++ app/services/mirakl/get_shipping_orders.rb
@@ -0,0 +1,48 @@
+module Mirakl
+  class GetShippingOrders < ApplicationService
+
+    attr_reader :stores
+
+    def initialize(args = {})
+      super
+      @stores = args[:stores]
+    end
+
+    def call
+      begin
+        @stores.each do |store|
+          orders = get_ready_orders(store)
+          build_orders(orders, store)
+        end
+      rescue ServiceError => error
+        add_to_errors(error.messages)
+      end
+
+      return completed_without_errors?
+    end
+
+    def get_ready_orders(store)
+      request = SpreeMirakl::Request.new(store).get("/api/orders?order_state_codes=SHIPPING&shop_id=#{store.shop_id}")
+      if request.success?
+        begin
+          return JSON.parse(request.body, {symbolize_names: true})[:orders]
+        rescue
+          raise ServiceError.new(["Error in getting Waiting Acceptance"])
+        end
+      else
+        raise ServiceError.new(["Error in getting Waiting Acceptance"])
+      end
+    end
+
+
+    def build_orders(orders, store)
+      orders.each do |order|
+        service = Mirakl::BuildOrder.new({mirakl_order_id: order[:order_id], store: store})
+        unless service.call
+          raise ServiceError.new(["Error processing order: #{order[:order_id]}"])
+        end
+      end
+    end
+
+  end
+end
