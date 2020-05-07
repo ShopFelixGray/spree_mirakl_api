a914e5aa3760e310b030429c6c085809f0514838
Mike Walters <mikewalters015@gmail.com>
2020-05-07 12:35:03 -0400

    Fix Mirakl Ship Jobs

--- app/jobs/mirakl_ship_job.rb
+++ app/jobs/mirakl_ship_job.rb
@@ -1,10 +1,10 @@
 class MiraklShipJob < ActiveJob::Base
   queue_as :default
 
-  def perform(store, order_id)
+  def perform(store, order_id, shipping_method, tracking_url)
     mirakl_request = SpreeMirakl::Request.new(store)
     request = mirakl_request.put("/api/orders/#{order_id}/tracking?shop_id=#{store.shop_id}",({  'carrier_name': shipping_method.try(:name), 'carrier_url': tracking_url }).to_json)
-    ship_request = mirakl_request.put("/api/orders/#{order_id}/ship?shop_id=#{store.shop_id}")
+    ship_request = mirakl_request.put("/api/orders/#{order_id}/ship?shop_id=#{store.shop_id}", '')
     unless request.success? && ship_request.success?
       raise Exception.new('Issue with pushing tracking info to Mirakl. Confirm in Mirakl tracking info exist and item is shipped')
     end
