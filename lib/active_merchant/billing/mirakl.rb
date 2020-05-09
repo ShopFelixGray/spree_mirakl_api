010ef57a5ac83ba5a77daed1b5f1a13cee824572
Mike Walters <mikewalters015@gmail.com>
2020-05-08 20:50:37 -0400

    Rework cancel and void:

--- lib/active_merchant/billing/mirakl.rb
+++ lib/active_merchant/billing/mirakl.rb
@@ -24,39 +24,6 @@ module ActiveMerchant #:nodoc:
         ActiveMerchant::Billing::Response.new(true, "", {}, {})
       end
 
-      def cancel(mirakl_source, options={})
-        void(mirakl_source, options)
-      end
-
-      def void(mirakl_source, options = {})
-        return_json = []
-        # Because spree doesn't use a reason code for cancel we just send the first reason 
-        # TODO: Confirm that is okay
-        transaction = Spree::MiraklTransaction.find_by(mirakl_order_id: mirakl_source)
-        transaction.order.line_items.each do |line_item|
-          mirakl_order_line = line_item.mirakl_order_line
-          return_json << {  'amount': line_item.amount, 
-            'order_line_id': mirakl_order_line.mirakl_order_line_id, 
-            'shipping_amount': 0,
-            'taxes': taxes_json(mirakl_order_line.mirakl_order_line_taxes.taxes, 1),
-            'reason_code': transaction.mirakl_store.mirakl_refund_reasons.first.code,
-            'shipping_taxes': taxes_json(mirakl_order_line.mirakl_order_line_taxes.shipping_taxes, 1),
-            'quantity': line_item.quantity,
-            'currency_iso_code': transaction.order.currency
-          }
-        end
-    
-        request = SpreeMirakl::Request.new(transaction.mirakl_store).put("/api/orders/refund?shop_id=#{transaction.mirakl_store.shop_id}", ({ 'refunds': return_json }).to_json)
-    
-        # We have to do it this way because if it is a success parsed response will have refunds
-        # if it fails we get message
-        if request.success?
-          ActiveMerchant::Billing::Response.new(true, "", request.parsed_response, {})
-        else 
-          ActiveMerchant::Billing::Response.new(false, request.parsed_response['message'][0...255], {}, {})
-        end
-      end
-
       def refund(money, mirakl_source, options = {})
         credit(money, mirakl_source, options = {})
       end
