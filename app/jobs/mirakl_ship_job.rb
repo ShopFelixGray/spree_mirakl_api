f1bcfb538a6620829d66d44f1b7c6215a637c049
Mike Walters <mikewalters015@gmail.com>
2020-05-11 11:01:40 -0400

    rename var

--- app/jobs/mirakl_ship_job.rb
+++ app/jobs/mirakl_ship_job.rb
@@ -3,7 +3,7 @@ class MiraklShipJob < ActiveJob::Base
 
   def perform(store, order_id, shipping_method, tracking_url)
     mirakl_request = SpreeMirakl::Api.new(store)
-    request = mirakl_request.tracking(mirakl_order_id, shipping_method, tracking_url)
+    request = mirakl_request.tracking(order_id, shipping_method, tracking_url)
     ship_request = mirakl_request.ship(order_id)
     unless request.success? && ship_request.success?
       raise Exception.new('Issue with pushing tracking info to Mirakl. Confirm in Mirakl tracking info exist and item is shipped')
