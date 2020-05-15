9ff90cfce02c89933447d82ba0825814a422d7d6
Mike Walters <mikewalters015@gmail.com>
2020-05-15 14:51:05 -0400

    Update for discounited and payment changes:

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,7 +1,12 @@
 Spree::Variant.class_eval do
 
   def quantity_check
-    self.total_on_hand > 0 ? self.total_on_hand : 0
+    # If item is no longer available remove all stock
+    if self.available?
+      self.total_on_hand > 0 ? self.total_on_hand : 0
+    else
+      0
+    end
   end
 
 end
