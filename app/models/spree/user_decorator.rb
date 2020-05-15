79155be420c53398c660407e14cfa9917470b083
Mike <mikewalters015@gmail.com>
2020-05-15 19:12:59 -0400

    Cancel Fix (#22)
    * read params
    
    * Add require for httparty
    
    * Add httparty to gemspec
    
    * Update for discounited and payment changes:
    
    * Remove double check
    
    * Remove double check
    
    * Remove double check
    
    * Fix refunds
    
    * Fix cancel

--- app/models/spree/user_decorator.rb
+++
@@ -1,3 +0,0 @@
-Spree::User.class_eval do
-  has_many :mirakl_store
-end
--- app/models/spree_mirakl/api.rb
+++ app/models/spree_mirakl/api.rb
@@ -38,6 +38,10 @@ class SpreeMirakl::Api
     @response = SpreeMirakl::Request.new(@store).put("/api/orders/refund?shop_id=#{@store.shop_id}", ({ 'refunds': return_json }).to_json)
   end
 
+  def cancel(return_json)
+    @response = SpreeMirakl::Request.new(@store).put("/api/orders/cancel?shop_id=#{@store.shop_id}", ({ 'cancelations': return_json }).to_json)
+  end
+
   def offers
     @response = SpreeMirakl::Request.new(@store).get("/api/offers?shop_id=#{@store.shop_id}")
   end
