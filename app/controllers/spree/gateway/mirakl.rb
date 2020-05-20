f3c3bfe2c9f00e756bd1f5fcc66f88080c382efe
Mike Walters <mikewalters015@gmail.com>
2020-05-19 23:31:28 -0400

    Add cancel tested with madewell and urban

--- app/controllers/spree/gateway/mirakl.rb
+++ app/controllers/spree/gateway/mirakl.rb
@@ -22,28 +22,13 @@ module Spree
     end
     
     def cancel(mirakl_source, options={})
-      return_json = []
-      # Because spree doesn't use a reason code for cancel we just send the first reason 
-      # TODO: Confirm that is okay
       transaction = Spree::MiraklTransaction.find_by(mirakl_order_id: mirakl_source)
-      transaction.order.line_items.each do |line_item|
-        mirakl_order_line = line_item.mirakl_order_line
-        return_json << {  'amount': line_item.amount, 
-          'order_line_id': mirakl_order_line.mirakl_order_line_id, 
-          'shipping_amount': 0,
-          'taxes': taxes_json(mirakl_order_line.mirakl_order_line_taxes.taxes),
-          'reason_code': transaction.mirakl_store.mirakl_refund_reasons.first.code,
-          'shipping_taxes': taxes_json(mirakl_order_line.mirakl_order_line_taxes.shipping_taxes),
-          'quantity': line_item.quantity,
-          'currency_iso_code': transaction.order.currency
-        }
-      end
   
-      request = SpreeMirakl::Api.new(transaction.mirakl_store).cancel(return_json)
+      request = SpreeMirakl::Api.new(transaction.mirakl_store).cancel(transaction.mirakl_order_id)
       # We have to do it this way because if it is a success parsed response will have refunds
       # if it fails we get message
       if request.success?
-        ActiveMerchant::Billing::Response.new(true, "", request.parsed_response, {})
+        ActiveMerchant::Billing::Response.new(true, "", {}, {})
       else 
         ActiveMerchant::Billing::Response.new(false, request.parsed_response['message'][0...255], {}, {})
       end
