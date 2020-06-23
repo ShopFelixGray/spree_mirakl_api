c28023be135f557830847ba3bde23796f3de14dc
Mike Walters <mikewalters015@gmail.com>
2020-06-23 16:03:40 -0400

    FGR-3629 edit ship worker

--- app/jobs/mirakl_ship_job.rb
+++ app/jobs/mirakl_ship_job.rb
@@ -5,13 +5,18 @@ class MiraklShipJob < ActiveJob::Base
     store = Spree::MiraklStore.find(store_id)
     shipment = Spree::Shipment.find(shipment_id)
     mirakl_request = SpreeMirakl::Api.new(store)
+    get_order = mirakl_request.get_order(order_id)
+    raise ServiceError.new(["Issue getting #{order_id}"]) unless get_order.success?
+    order_data = JSON.parse(get_order.body, symbolize_names: true)[:orders][0]
 
     request = mirakl_request.tracking(order_id,
                                       shipment.shipping_method,
                                       shipment.tracking_url)
-    ship_request = mirakl_request.ship(order_id)
-    unless request.success? && ship_request.success?
-      raise Exception.new(Spree.t(:shipping_fail))
+    # If an order is already marked as shipped dont reship it
+    if order_data[:order_state] != 'SHIPPING'
+      ship_request = mirakl_request.ship(order_id)
+      raise Exception.new(Spree.t(:shipping_fail)) unless ship_request.success?
     end
+    raise Exception.new(Spree.t(:shipping_fail)) unless request.success?
   end
 end
