8b54aaf1970394828927c7bdf8b9e709d18f3048
Mike Walters <mikewalters015@gmail.com>
2020-08-26 20:05:47 -0400

    FGR-3641 remove pointless space

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   namespace :admin do
-    
-
     resources :mirakl_stores do
       get :refresh_inventory
       get :reason_mapper
